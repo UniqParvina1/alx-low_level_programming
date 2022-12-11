6061e782d7c880c692482a024f20cb372be2d540
Kamilat Olufolajimi <kamilatolu@gmail.com>
2022-12-11 10:36:47 -0800

    t100 edit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,24 +18,24 @@ int main(void)
 	i = 0;
 
 	while (i < 100)
-	{
+	
 		j = i % 10; /* singles digit */
 		k = i / 10; /* doubles digit */
-												if (k < j)
-		{
-			putchar(k + '0');
-			putchar(j + '0');
 
-			if (i < 89)
-			{
-														putchar(44);
-				putchar(32);								}
+		if (k < j)
+		{
+													putchar(k + '0');
+													putchar(j + '0');
+
+													if (i < 89)
+			{																					putchar(44);
+				putchar(32);
+			}
 		}
 
-												i++;
+			i++;
+		}
+		putchar('\n');
 
-	}
-	putchar('\n');
-
-	Return (0);
+		return (0);
 }
